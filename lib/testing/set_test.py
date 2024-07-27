66897214f36f959c9ad5b1bdb3f6c7a066538248
Irene Mwova <irene.mwova@student.moringaschool.com>
2024-07-27 11:56:13 +0300

    done

--- lib/testing/set_test.py
+++ lib/testing/set_test.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
-from MySet import MySet
+import sys
+import os
+
+# Add the project root directory to the Python path
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.insert(0, project_root)
+
+from lib.MySet import MySet
 
 class TestSet:
 
